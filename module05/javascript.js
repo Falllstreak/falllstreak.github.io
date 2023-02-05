09aca1e5f60f3b1e18bfda5300a3bc061ac297fc
Falllstreak <121982351+Falllstreak@users.noreply.github.com>
2023-02-04 20:50:38 -0800

    Update javascript.js

--- module05/javascript.js
+++ module05/javascript.js
@@ -1,10 +1,11 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
+var number= prompt("Enter a Number", "0 to 10");        //Ask user for a number
+var table = number;             // Unit of table
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = '<h2>Multiplication Table</h2>';              // Message
 
 if (operator === 'addition') {
-  // Do addition
+  // Do multiplication
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
