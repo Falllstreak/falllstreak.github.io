c2e63f3edef73f5395ddb4df3ef0c787e6cc455d
Falllstreak <121982351+Falllstreak@users.noreply.github.com>
2023-02-22 14:41:23 -0800

    Update JavaScript.js

--- artifact05/JavaScript.js
+++ artifact05/JavaScript.js
@@ -1,7 +1,7 @@
 //Let's define our variables
 i = 0;
 var myImages = ["https://user-images.githubusercontent.com/121982351/220744728-0294e680-c0c3-4eff-a26b-c9d9ac5b8a06.jpeg", "https://user-images.githubusercontent.com/121982351/220744508-69da9686-759f-4e5f-827a-ca5cc9c87754.jpeg", "https://user-images.githubusercontent.com/121982351/220744121-5e0c9403-33fd-4a3f-a1ce-9a3ab27d8ca6.jpeg", "https://user-images.githubusercontent.com/121982351/220743967-963ad763-b112-4845-a400-ef69205a221e.jpeg", "https://user-images.githubusercontent.com/121982351/220743867-0d400e77-d44b-45e4-85e9-f66f30fedb87.jpeg"];
-var myPhrases = ["Blooming", "Rest", "Mountains", "City", "Focus"];
+var myPhrases = ["Blooming", "Resting Snow", "Mountain Road", "City Glow", "Focus"];
 window.setInterval(nextButton, 4000);
 
 
